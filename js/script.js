001e745a75ea9c529aa5e494dd03019f3ac2a1f2
Sean <sean.a.blackman@student.uncommonhs.org>
2017-12-07 22:33:34 +0000

    Fixed CSS

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 //Class 1:
         // Fork and clone the repo and set up your project workspace and link files
         // Write HTML for your project
-//Class 2:  
+//Class 2:
         // Declare four global variables for intial state of the list of choices, the computer choice, the user choice, and the winner.  
         // Practice using console.log() and debugger to debug your code
         // Create your document ready function. 
         // Create a click hander for when the "Shoot!" button is clicked, takes in the user choice from the input field, 
         // Display the user choice to the output screen
-//Class 3:  
+//Class 3:
         // Randomly choose among 'rock', 'paper', or 'scissors' from the list of choices 
         // Display the computer choice to the output screen
         // Compare the user choice and the computer choice to determine who won. 
@@ -19,8 +19,10 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
 //GLOBAL VARIABLES
-
-
+var userChoice = "";
+var computerChoice = "";
+var winner = "";
+var choices = ["rock","paper","scissors"];
 
 //FUNCTIONS
 
